130392ac01f6ddc0e0540424007bec64cd6f0c5f
Mxyuki <86327219+Mxyuki@users.noreply.github.com>
2022-12-06 01:18:05 +0100

    Generation V2
    -Added Cursor
    -Added place/destroy blocks
    -Added Inventory

--- Generation.c
+++ Generation.c
@@ -1,5 +1,5 @@
 // Author   : Mxyuki
-// Version  : 1.0
+// Version  : 2.0
 // Compil   : gcc -Wall Gene.c -o Gene
 
 #include <stdio.h>
@@ -16,6 +16,11 @@ int medPerc = 98;
 int posx = 0;
 int posy = 0;
 
+int curX = 0;
+int curY = 0;
+
+int inv = 0;
+
 time_t t;
 
 int main (){
@@ -29,7 +34,7 @@ int main (){
 
 void GenTab(){
 
-    int tab[taillex][tailley];
+    int tab[taillex][tailley]; 
 
     while(posy < tailley){
         while(posx < taillex){
@@ -240,4 +245,79 @@ void GenTab(){
         posx = 0;
         posy++;
     }
+    cursor(tab);
+}
+
+
+
+void cursor(int tab[][tailley]){
+
+    int c;
+
+    system ("/bin/stty raw");
+    while((c=getchar())!= 27) {
+       
+        if(c == '8') curY--;
+        if(c == '4') curX--;
+        if(c == '6') curX++;
+        if(c == '5') curY++; 
+
+        if(curX < 0) curX++;
+        if(curX == taillex) curX--;
+        if(curY < 0)curY++;
+        if(curY == tailley)curY--;
+
+        if(c == '8' || c == '4' || c == '6' || c == '5'){
+
+            system("@cls||clear");
+
+            system("/bin/stty cooked");
+
+            printf("\n");
+
+            while(posy < tailley){
+
+                while(posx < taillex){
+            
+                    if(posx == curX && posy == curY){ 
+                        printf("[]");
+                        posx++;
+                    }
+
+                    else{
+
+                        if(tab[posy][posx] == 1) printf("▓▓");
+                        else if(tab[posy][posx] == 2) printf("▒▒");
+                        else if(tab[posy][posx] == 3) printf("░░");
+
+                        posx++;
+                    }
+                }
+                printf("\n");
+                posx = 0;
+                posy++;
+            }
+            printf("Inventory : %d\n", inv);
+            posx = 0;
+            posy = 0;
+            system ("/bin/stty raw"); 
+            c = 0;
+        }
+
+        else if(c == '9'){
+            if(tab[curY][curX] != 3 && inv > 0){ 
+                tab[curY][curX] = tab[curY][curX] + 1;
+                inv = inv - 1;
+            }
+        }
+        else if(c == '7'){
+            if(tab[curY][curX] != 1){ 
+                tab[curY][curX] = tab[curY][curX] - 1;
+                inv = inv + 1;
+            }
+        }
+    }
+
+    system("/bin/stty cooked");
+  
 }
